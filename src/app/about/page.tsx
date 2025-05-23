b7d62c466d8fcb5e42678aa602a0171aaa79c0d2
Tina Huynh <tinamchuynh@gmail.com>
2025-05-23 14:32:45 -0700

    Increases bottom padding for multiple pages to enhance layout consistency

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function AboutPage() {
   return (
-    <div className="mx-auto pt-8 md:pt-12 lg:pt-16 w-10/12 md:w-11/12">
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-24 w-10/12 md:w-11/12">
       About Page
     </div>
   );
--- src/app/resources/parent-guide/page.tsx
+++ src/app/resources/parent-guide/page.tsx
@@ -1,6 +1,6 @@
 export default function ParentGuidePage() {
   return (
-    <div className="mx-auto pt-8 md:pt-12 lg:pt-16 w-10/12 md:w-11/12">
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-24 w-10/12 md:w-11/12">
       Parent Guide Page
     </div>
   );
