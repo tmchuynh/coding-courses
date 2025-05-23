97d0b467e55d8af9575b6ded4094365408b9e76c
Tina Huynh <tinamchuynh@gmail.com>
2025-05-23 14:32:01 -0700

    Refactors import paths for Header and Footer components and adjusts main margin for layout consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "@/components/navigation/Footer";
+import Header from "@/components/navigation/Header";
 import DynamicBreadcrumb from "@/components/navigation/dynamic-breadcrumb";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="relative flex flex-col min-h-screen">
         <Providers>
-          <main className="flex-grow md:pb-0">
+          <main className="flex-grow mt-24 md:pb-0">
             <DynamicBreadcrumb />
             <Header />
             {children}
