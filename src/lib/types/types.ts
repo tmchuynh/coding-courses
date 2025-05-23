47fdf824d5bde5c50941591c7cff25eb920e19f6
Tina Huynh <tinamchuynh@gmail.com>
2025-05-22 23:46:25 -0700

    Adds new type definitions for testimonials and courses
    Introduces several new type definitions to enhance data structure representation, including `Testimonial`, `Subsection`, `Stories`, `FAQs`, `FAQSection`, `Course`, `Schedule`, and `CourseSchedule`.
    
    These additions improve the clarity and organization of course-related data, facilitating better management and usage throughout the application.

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -2,3 +2,59 @@ export type NotFoundContextType = {
   isNotFound: boolean;
   setNotFound: (value: boolean) => void;
 };
+
+export type Testimonial = {
+  testimonial: string;
+  author: string;
+};
+
+export type Subsection = {
+  id?: number;
+  title: string;
+  points?: string[];
+  description?: string;
+};
+
+export type Stories = {
+  name: string;
+  grade: string;
+  location: string;
+  quote: string;
+  before: string;
+  during: string;
+  after: string;
+  keyOutcomes: string;
+};
+
+export type FAQs = {
+  question: string;
+  answer: string;
+};
+
+export type FAQSection = {
+  title: string;
+  faqs: FAQs[];
+};
+
+export type Course = {
+  courseName: string;
+  duration?: string;
+  format?: string;
+  keyProjects?: string[];
+  description?: string;
+  focus?: string[];
+  outcomes: string[];
+  next?: string[];
+};
+
+export type Schedule = {
+  duration: string;
+  weeklySessions: string;
+  sessionLength: string;
+  options: string[];
+};
+
+export type CourseSchedule = {
+  ageGroup: string;
+  schedules: CourseSchedule[];
+};
