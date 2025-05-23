e6cfa90d6aafec98a0465deda25b599e9a928c45
Tina Huynh <tinamchuynh@gmail.com>
2025-05-23 14:32:57 -0700

    Increases bottom padding for Age Groups page for improved layout consistency

--- src/app/curriculum/age-groups/page.tsx
+++ src/app/curriculum/age-groups/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function AgeGroupsPage() {
   return (
-    <div className="mx-auto pt-8 md:pt-12 lg:pt-16 w-10/12 md:w-11/12">
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-24 w-10/12 md:w-11/12">
       <h1>By Age Group Page</h1>
     </div>
   );
