4cb52bc391209a950c76a85f849e7e2a076041e3
Tina Huynh <tinamchuynh@gmail.com>
2025-05-23 02:14:30 -0700

    feat: add new pages for Careers, Impact, Mission, Team, Contact, and Curriculum

---
+++ src/app/about/team/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamPage() {
+  return (
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-16 w-10/12 md:w-11/12">
+      Our Team Page
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <div className="mx-auto pt-8 md:pt-12 lg:pt-16 w-10/12 md:w-11/12">
+      Contact Page
+    </div>
+  );
+}
