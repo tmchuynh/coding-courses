b8215f7a56a05dad95e62dc3183fe037679a26f4
Tina Huynh <tinamchuynh@gmail.com>
2025-05-23 14:38:21 -0700

    Enhances Home page layout with new components, improved structure, and updated styling for better user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   ArrowPathIcon,
   ChevronRightIcon,
@@ -7,6 +9,7 @@ import {
   LockClosedIcon,
   ServerIcon,
 } from "@heroicons/react/20/solid";
+import Image from "next/image";
 
 const primaryFeatures = [
   {
@@ -90,14 +93,9 @@ export default function Home() {
           <div className="lg:flex lg:items-center lg:gap-x-10 mx-auto px-6 lg:px-8 py-24 sm:py-32 lg:py-40 max-w-7xl">
             <div className="lg:flex-auto mx-auto lg:mx-0 max-w-2xl">
               <div className="flex">
-                <div className="relative flex items-center gap-x-4 bg-white px-4 py-1 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20 text-gray-600 text-sm/6">
-                  <span className="font-semibold text-indigo-600">
-                    We’re hiring
-                  </span>
-                  <span
-                    aria-hidden="true"
-                    className="bg-gray-900/10 w-px h-4"
-                  />
+                <div className="relative flex items-center gap-x-4 bg-white px-4 py-1 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20 text-sm/6">
+                  <span className="font-semibold">We’re hiring</span>
+                  <span aria-hidden="true" className="w-px h-4 /10" />
                   <a href="#" className="flex items-center gap-x-1">
                     <span aria-hidden="true" className="absolute inset-0" />
                     See open positions
@@ -108,7 +106,7 @@ export default function Home() {
                   </a>
                 </div>
               </div>
-              <h1 className="mt-10 font-semibold text-5xl text-gray-900 text-pretty sm:text-7xl tracking-tight">
+              <h1 className="mt-10 font-semibold text-5xl text-pretty sm:text-7xl tracking-tight">
                 A better way to ship your projects
               </h1>
               <p className="mt-8 font-medium text-gray-500 text-lg text-pretty sm:text-xl/8">
@@ -120,11 +118,11 @@ export default function Home() {
               <div className="flex items-center gap-x-6 mt-10">
                 <a
                   href="#"
-                  className="bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-sm text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
                 </a>
-                <a href="#" className="font-semibold text-gray-900 text-sm/6">
+                <a href="#" className="font-semibold text-sm/6">
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
@@ -155,9 +153,11 @@ export default function Home() {
                   clipPath="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)"
                   transform="translate(24 24)"
                 >
-                  <img
+                  <Image
                     alt=""
                     src="https://tailwindcss.com/plus-assets/img/component-images/mobile-app-screenshot.png"
+                    width={316}
+                    height={684}
                   />
                 </foreignObject>
               </svg>
@@ -168,35 +168,35 @@ export default function Home() {
         {/* Logo cloud */}
         <div className="mx-auto px-6 lg:px-8 max-w-7xl">
           <div className="items-center gap-x-8 gap-y-12 sm:gap-x-10 sm:gap-y-14 grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-5 mx-auto lg:mx-0 max-w-lg sm:max-w-xl lg:max-w-none">
-            <img
+            <Image
               alt="Transistor"
               src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-gray-400.svg"
               width={158}
               height={48}
               className="col-span-2 lg:col-span-1 w-full max-h-12 object-contain"
             />
-            <img
+            <Image
               alt="Reform"
               src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-400.svg"
               width={158}
               height={48}
               className="col-span-2 lg:col-span-1 w-full max-h-12 object-contain"
             />
-            <img
+            <Image
               alt="Tuple"
               src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-400.svg"
               width={158}
               height={48}
               className="col-span-2 lg:col-span-1 w-full max-h-12 object-contain"
             />
-            <img
+            <Image
               alt="SavvyCal"
               src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-400.svg"
               width={158}
               height={48}
               className="col-span-2 lg:col-span-1 sm:col-start-2 w-full max-h-12 object-contain"
             />
-            <img
+            <Image
               alt="Statamic"
               src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-400.svg"
               width={158}
@@ -208,19 +208,19 @@ export default function Home() {
 
         {/* Feature section */}
         <div className="mx-auto mt-32 sm:mt-56 sm:px-6 lg:px-8 max-w-7xl">
-          <div className="relative bg-gray-900 px-6 sm:px-10 xl:px-24 py-20 sm:py-24 lg:py-24 sm:rounded-3xl overflow-hidden isolate">
+          <div className="relative px-6 sm:px-10 xl:px-24 py-20 sm:py-24 lg:py-24 sm:rounded-3xl overflow-hidden isolate">
             <div className="lg:items-center gap-x-8 gap-y-16 sm:gap-y-20 lg:gap-y-0 grid grid-cols-1 lg:grid-cols-2 mx-auto lg:mx-0 max-w-2xl lg:max-w-none">
               <div className="lg:row-start-2 lg:max-w-md">
-                <h2 className="font-semibold text-3xl text-balance text-white sm:text-4xl tracking-tight">
+                <h2 className="font-semibold text-3xl text-balance sm:text-4xl tracking-tight">
                   Boost your productivity. Start using our app today.
                 </h2>
-                <p className="mt-6 text-gray-300 text-lg/8">
+                <p className="mt-6 text-lg/8">
                   Ac euismod vel sit maecenas id pellentesque eu sed
                   consectetur. Malesuada adipiscing sagittis vel nulla. Ac
                   euismod vel sit maecenas.
                 </p>
               </div>
-              <img
+              <Image
                 alt="Product screenshot"
                 src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
                 width={2432}
@@ -228,10 +228,10 @@ export default function Home() {
                 className="relative -z-20 lg:row-span-4 shadow-xl rounded-xl ring-1 ring-white/10 lg:w-5xl min-w-full max-w-xl lg:max-w-none"
               />
               <div className="lg:row-start-3 lg:mt-10 lg:pt-10 lg:border-white/10 lg:border-t max-w-xl lg:max-w-md">
-                <dl className="space-y-8 max-w-xl lg:max-w-none text-base/7 text-gray-300">
+                <dl className="space-y-8 max-w-xl lg:max-w-none text-base/7">
                   {primaryFeatures.map((feature) => (
                     <div key={feature.name} className="relative">
-                      <dt className="inline-block ml-9 font-semibold text-white">
+                      <dt className="inline-block ml-9 font-semibold">
                         <feature.icon
                           aria-hidden="true"
                           className="top-1 left-1 absolute text-indigo-500 size-5"
@@ -244,31 +244,17 @@ export default function Home() {
                 </dl>
               </div>
             </div>
-            <div
-              aria-hidden="true"
-              className="top-1/2 lg:top-auto lg:-bottom-48 left-12 -z-10 absolute blur-3xl transform-gpu lg:transform-gpu -translate-y-1/2 lg:translate-y-0 pointer-events-none"
-            >
-              <div
-                style={{
-                  clipPath:
-                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-                }}
-                className="bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-25 w-288.75 aspect-1155/678"
-              />
-            </div>
           </div>
         </div>
 
         {/* Feature section */}
         <div className="mx-auto mt-32 sm:mt-56 px-6 lg:px-8 max-w-7xl">
           <div className="mx-auto max-w-2xl lg:text-center">
-            <h2 className="font-semibold text-base/7 text-indigo-600">
-              Deploy faster
-            </h2>
-            <p className="mt-2 font-semibold text-4xl text-gray-900 text-pretty sm:text-5xl lg:text-balance tracking-tight">
+            <h2>Deploy faster</h2>
+            <p className="mt-2 font-semibold text-4xl text-pretty sm:text-5xl lg:text-balance tracking-tight">
               Everything you need to deploy your app
             </p>
-            <p className="mt-6 text-gray-600 text-lg/8">
+            <p className="mt-6">
               Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
               Suspendisse eget egestas a elementum pulvinar et feugiat blandit
               at. In mi viverra elit nunc.
@@ -278,19 +264,19 @@ export default function Home() {
             <dl className="gap-x-8 gap-y-16 grid grid-cols-1 lg:grid-cols-3 max-w-xl lg:max-w-none">
               {secondaryFeatures.map((feature) => (
                 <div key={feature.name} className="flex flex-col">
-                  <dt className="flex items-center gap-x-3 font-semibold text-base/7 text-gray-900">
+                  <dt className="flex items-center gap-x-3 font-semibold text-base/7">
                     <feature.icon
                       aria-hidden="true"
-                      className="flex-none text-indigo-600 size-5"
+                      className="flex-none text-primary size-5"
                     />
                     {feature.name}
                   </dt>
-                  <dd className="flex flex-col flex-auto mt-4 text-base/7 text-gray-600">
+                  <dd className="flex flex-col flex-auto mt-4 text-base/7">
                     <p className="flex-auto">{feature.description}</p>
                     <p className="mt-6">
                       <a
                         href={feature.href}
-                        className="font-semibold text-indigo-600 text-sm/6"
+                        className="font-semibold text-sm/6"
                       >
                         Learn more <span aria-hidden="true">→</span>
                       </a>
@@ -304,60 +290,34 @@ export default function Home() {
 
         {/* Newsletter section */}
         <div className="mx-auto mt-32 sm:mt-56 sm:px-6 lg:px-8 max-w-7xl">
-          <div className="relative bg-gray-900 shadow-2xl px-6 sm:px-24 py-24 xl:py-32 sm:rounded-3xl overflow-hidden isolate">
-            <h2 className="mx-auto max-w-3xl font-semibold text-4xl text-center text-white sm:text-5xl tracking-tight">
+          <div className="relative bg-card shadow px-6 sm:px-24 py-24 xl:py-32 border border-border sm:rounded-3xl text-card-foreground overflow-hidden isolate">
+            <h2 className="mx-auto max-w-3xl text-center">
               Get notified when we’re launching
             </h2>
-            <p className="mx-auto mt-6 max-w-lg text-center text-gray-300 text-lg">
+            <p className="mx-auto mt-6 max-w-lg text-center text-lg">
               Reprehenderit ad esse et non officia in nulla. Id proident tempor
               incididunt nostrud nulla et culpa.
             </p>
             <form className="flex gap-x-4 mx-auto mt-10 max-w-md">
-              <label htmlFor="email-address" className="sr-only">
+              <Label htmlFor="email-address" className="sr-only">
                 Email address
-              </label>
-              <input
+              </Label>
+              <Input
                 id="email-address"
                 name="email"
                 type="email"
                 required
                 placeholder="Enter your email"
                 autoComplete="email"
-                className="flex-auto bg-white/5 px-3.5 py-2 rounded-md min-w-0 text-base text-white sm:text-sm/6 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-white"
+                className="flex-auto bg-white/5 px-3.5 py-2 rounded-md min-w-0 text-base sm:text-sm/6 placeholder:text-gray-400 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-white"
               />
               <button
                 type="submit"
-                className="flex-none bg-white hover:bg-gray-100 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-gray-900 text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                className="flex-none bg-white hover:bg-gray-100 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Notify me
               </button>
             </form>
-            <svg
-              viewBox="0 0 1024 1024"
-              aria-hidden="true"
-              className="top-1/2 left-1/2 -z-10 absolute -translate-x-1/2 size-256"
-            >
-              <circle
-                r={512}
-                cx={512}
-                cy={512}
-                fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
-                fillOpacity="0.7"
-              />
-              <defs>
-                <radialGradient
-                  r={1}
-                  cx={0}
-                  cy={0}
-                  id="759c1415-0410-454c-8f7c-9a820de03641"
-                  gradientUnits="userSpaceOnUse"
-                  gradientTransform="translate(512 512) rotate(90) scale(512)"
-                >
-                  <stop stopColor="#7775D6" />
-                  <stop offset={1} stopColor="#E935C1" stopOpacity={0} />
-                </radialGradient>
-              </defs>
-            </svg>
           </div>
         </div>
       </main>
