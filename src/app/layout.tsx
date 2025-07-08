08ce5825fef3b0c3e0d0de062daff6148a047b17
Tina Huynh <tinamchuynh@gmail.com>
2025-07-08 10:18:32 -0700

    Add metadata for SEO and adjust layout spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
-import Footer from "@/components/navigation/Footer";
 import Header from "@/components/navigation/Header";
 import DynamicBreadcrumb from "@/components/navigation/dynamic-breadcrumb";
+import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: "Coding Courses - Learn to Code, Shape Your Future",
+  description:
+    "Empower your child with essential digital skills through our expert-led coding classes. From elementary school to high school, our age-appropriate curriculum builds confidence, creativity, and technical skills that last a lifetime.",
+};
 
 export default function RootLayout({
   children,
@@ -13,11 +19,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="relative flex flex-col min-h-screen">
         <Providers>
-          <main className="flex-grow mt-24 md:pb-0">
+          <main className="flex-grow my-24 md:pb-0">
             <DynamicBreadcrumb />
             <Header />
             {children}
-            <Footer />
           </main>
         </Providers>
       </body>
